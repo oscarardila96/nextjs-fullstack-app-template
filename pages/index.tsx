766c4d8eac281eb5e3359fa37166e550f8153d50
Henry Azogue <hnrazogue@gmail.com>
2023-02-16 20:55:59 -0500

    fix: second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-red-200">HOME</div>
+      <div className="bg-red-200">
+        HOME
+        <h1>hola</h1>
+      </div>
     </>
   );
 }
---
+++ pages/sobre/me.tsx
@@ -0,0 +1,12 @@
+const Me = () => {
+  return (
+    <div>
+      <h1>Mi</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt odit
+        voluptatibus nisi molestiae debitis consequuntur!
+      </p>
+    </div>
+  );
+};
+export default Me;
